918f6f0819dd3d081bdbae8d081986da8bda4a30
Roniery Silva <roniery.js@outlook.com>
2024-06-21 20:43:24 -0300

    Meu primeiro Commit

---
+++ main.py
@@ -0,0 +1,29 @@
+print("Jornada de dados")
+
+#Variáveis
+idade = 34
+nome = "Roniery"
+
+#Chamar as variáveis (Print)
+print(idade)
+
+#Soma
+nova_idade = idade + 1
+print(nova_idade)
+
+#Lista
+lista = (1,2,3,4,5)
+print(lista)
+
+#lista.reverse() mostrado em aula mais não funcionou
+#print(lista)
+
+#Métodos phyton
+lista_invertida = lista[::-1]
+print(lista_invertida)
+
+
+
+
+
+
